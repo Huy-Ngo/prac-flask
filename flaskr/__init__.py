837114654edab5ad6ecb19d5729de480c0cd6cf1
Ngo Anh <anhnn@foobla.com>
2019-07-14 22:20:57 +0700

    First commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,7 @@
+""" REST API with flask
+This is an exercise to build a REST API with flask (get, post, put, delete)
+Based on https://flask.palletsprojects.com/en/1.1.x/tutorial/
+"""
+
+from flask import Flask
+app = Flask(__name__)
